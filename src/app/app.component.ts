630ad8ac8c4b23fccb7b384741eb8070b775dd42
unknown <pradeep.bhosle8@gmail.com>
2017-02-07 19:39:45 +0530

    changeas angular cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MyNewComponentComponent} from './my-new-component/my-new-component.component';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[MyNewComponentComponent]
+  
 })
 export class AppComponent {
   title = 'app works!';
